9b970692ff065622608cf7f54d69395fa615ebee
kacper <kacpersciuba@gmail.com>
2026-09-10 00:43:39 +0200

    Add cat facts model

---
+++ models/CatFactDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CatFact.models;
+
+public class CatFactDto
+ {
+    [JsonPropertyName("fact")]
+    public string CatId { get; set; } = string.Empty;
+
+
+    [JsonPropertyName("lenght")]
+    public int Lenght { get; set; }
+
+ }
+
