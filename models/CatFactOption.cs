e3ba5c3210a7829b23e340ef3b17f5c069f68614
kacper <kacpersciuba@gmail.com>
2026-09-10 01:33:21 +0200

    add CatFactSettings

---
+++ models/CatFactOption.cs
@@ -0,0 +1,11 @@
+﻿namespace CatFact.models;
+
+public class CatFactOption
+{
+    public const string SectionName = "CatFactSettings";
+
+    public string ApiUrl { get; set; } = string.Empty;
+    public string FilePath { get; set; } = "cat_facts.txt";
+
+}
+
